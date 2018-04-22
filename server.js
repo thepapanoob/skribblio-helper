c05a754475d21a24951c4d85f644d61dce73b363
Pong Lenis <thepapanoob@users.noreply.github.com>
2018-04-22 15:10:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
+const https = require('https');
 const path = require('path');
 const app = express();
 
@@ -8,18 +9,23 @@ const app = express();
 const DRUCKBETANKUNG = false;
 const savePath = path.resolve(__dirname, 'wordlist.json');
 
-let wordlist = {};
-let correctWord = "";
+const sslOptions = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+  passphrase: 'aaaa'
+};
 
-if (fs.existsSync(savePath))
-{
+let wordlist = {};
+let correctWord = '';
+
+if (fs.existsSync(savePath)) {
   wordlist = JSON.parse(fs.readFileSync(savePath, 'utf8'));
 }
 
 app.use(multer().array());
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://skribbl.io');
+  res.setHeader('Access-Control-Allow-Origin', 'https://skribbl.io');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
@@ -33,37 +39,35 @@ app.use(function(req, res, next) {
 });
 
 function insertWord(word) {
-  if (wordlist[word.length] === undefined)
-  {
+  if (wordlist[word.length] === undefined) {
     wordlist[word.length] = [];
   }
 
-  if (wordlist[word.length].indexOf(word) === -1)
-  {
+  if (wordlist[word.length].indexOf(word) === -1) {
     wordlist[word.length].push(word);
   }
 }
 
 app.post('/', (req, res) => {
-  if (req.body && req.body.w)
-    insertWord(req.body.w);
+  if (req.body && req.body.w) insertWord(req.body.w);
 
-  if (req.body.s)
-    correctWord = req.body.w;
+  if (req.body.s) correctWord = req.body.w;
 
   res.send('ok');
 });
 
 app.get('/:regex', (req, res) => {
-  if (DRUCKBETANKUNG)
-  {
+  if (DRUCKBETANKUNG) {
     return res.send([correctWord]);
   }
 
   const replaceRegex = new RegExp('_', 'g');
 
   const regexString = req.params.regex.replace(replaceRegex, '[a-z]');
-  const filterList = wordlist[req.params.regex.length] !== undefined ? wordlist[req.params.regex.length] : [];
+  const filterList =
+    wordlist[req.params.regex.length] !== undefined
+      ? wordlist[req.params.regex.length]
+      : [];
   const filterRegex = new RegExp(regexString, 'g');
 
   let matchedItems = filterList.filter(value => {
@@ -77,28 +81,25 @@ app.get('/', (req, res) => {
   res.send('Hello skribbl.io!');
 });
 
-let server = app.listen(3000, () => {
+let server = https.createServer(sslOptions, app).listen(3000, () => {
   console.log('Skribbl.io Cheat server listening on port 3000!');
 });
 
 function gracefulShutdown() {
-  fs.writeFile(
-    savePath,
-    JSON.stringify(wordlist, null, 0),
-    function(err) {
-      if (err) {
-        return console.log(err);
-      }
-
-      server.close(() => {
-        process.exit();
-      });
-
-      setTimeout(() => {
-        process.exit();
-      }, 10000);
+  console.log('Shutdown');
+  fs.writeFile(savePath, JSON.stringify(wordlist, null, 0), function(err) {
+    if (err) {
+      return console.log(err);
     }
-  );
+
+    server.close(() => {
+      process.exit();
+    });
+
+    setTimeout(() => {
+      process.exit();
+    }, 10000);
+  });
 }
 
 process.on('SIGTERM', gracefulShutdown);
